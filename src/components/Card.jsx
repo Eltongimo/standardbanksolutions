c2c0ebfd1c05c11c3f5d5322bbe6f5f431d773bd
Eltongimo <eltonug@gmail.com>
2022-06-04 02:41:13 +0200

    some updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,17 @@
 import './Card.css'
 import React from 'react'
+import Modal from 'react-modal'
+import {useForm} from 'react-hook-form'
 
 function Card(props){
+    
+    const [showModalForm, setShowModalForm] = React.useState(false)
+    const {register, handleSubmit} = useForm() 
+    
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <div className='sticky-notes'>
             <div className='note-header'>
@@ -11,10 +21,69 @@ function Card(props){
                 {props.serviceDescription}
             </div>
             <div className='content-button'>
-                <button className='request-button'>
+                <button className='request-button' onClick={() => setShowModalForm(true)}>
                     solicitar senha
                 </button>
+
+                <Modal  className='modal-container' isOpen={showModalForm}
+                              onRequestClose={() => setShowModalForm(false)}
+                              style={
+                                    {
+                                        overlay:{
+                                            backgroundColor: 'rgb(204, 204, 204,0.9)',
+                                        },                                   
+                                    } 
+                            }
+                        >
+                        <form className='form-container' onSubmit={handleSubmit(onSubmit)}>
+                      
+                            <div className="form-body">
+                                <label> 
+                                    <div className='txt'>
+                                        Nome
+                                    </div> 
+                                    <div className="subject">
+                                        <input type="text" name="name" placeholder="Por favor insira o seu nome completo" />
+                                    </div>
+                                </label>
+                                
+                                <label> 
+                                    <div className='txt'>
+                                        Nome
+                                    </div> 
+                                    <div className="subject">
+                                        <input type="text" name="name" placeholder="Por favor insira o seu nome completo" />
+                                    </div>
+                                </label>
+                                
+                                
+                                <div className="subject">
+                                    <input type="text" name="name" placeholder="Por favor insira o seu nome completo" />
+                                </div>
+
+                                <div className="subject">
+                                    <input type="text" name="name" placeholder="Por favor insira o seu nome completo" />
+                                </div>
+    
+                                <select>
+                                    <option>option 1</option>
+                                    <option>option 1</option>
+                                    <option>option 1</option>
+                                    <option>option 1</option>
+                                </select>
+
+                            </div>
+                    
+                            <div className='modal-form-footer'>
+                                <div className='footer-btn'>
+                                    <button className='btn btn-success' onClick= {() => setShowModalForm(false)}>Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    
+                </Modal>
             </div>
+
         </div>
     )
 }
